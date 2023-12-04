d3e6776d92ff0f46aa731e839069e0af68bdcb80
MwendwaM <fisher1671999@gmail.com>
2023-12-04 12:10:07 +0300

    Main Contracts

---
+++ src/CLAMM.sol
@@ -0,0 +1,433 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import "./interfaces/IERC20.sol";
+import "./lib/Tick.sol";
+import "./lib/Position.sol";
+import "./lib/SafeCast.sol";
+import "./lib/TickMath.sol";
+import "./lib/SqrtPriceMath.sol";
+import "./lib/SwapMath.sol";
+import "./lib/TickBitMap.sol";
+
+function checkTicks(int24 tickLower, int24 tickUpper)  pure {
+    require(tickLower < tickUpper, ' lower tick larger than upper tick');
+    require(tickLower >= TickMath.MIN_TICK, 'tlm');
+    require(tickUpper <= TickMath.MAX_TICK, "TICK UPPER more than max tick");
+}
+
+
+contract CLAMM {
+    using SafeCast for int256;
+    using SafeCast for uint256;
+    using Tick for mapping(int24 => Tick.Info);
+    using TickBitMap for mapping (int16 => uint256);
+    using Position for mapping(bytes32 => Position.Info);
+    using Position for Position.Info;
+
+    address public immutable token0;
+    address public immutable token1;
+    uint24 public immutable fee;
+    int24 public immutable tickSpacing;
+
+    uint128 public immutable maxLiquidityPerTick;
+
+
+    struct Slot0 {
+        uint160 sqrtPriceX96;
+        int24 tick;
+        //uint8 feeProtocol;
+        bool unlocked;
+    }
+    Slot0 public  slot0;
+    uint256 public feeGrowthGlobal0X128;
+    uint256 public feeGrowthGlobal1X128;
+    uint128 public liquidity;
+    mapping(int24 => Tick.Info) public ticks;
+    mapping(int16 => uint256) public tickBitmap;
+    mapping(bytes32 => Position.Info) public positions; 
+
+
+    modifier lock(){
+        require(slot0.unlocked, " This is unlocked");
+        slot0.unlocked = false;
+        _;
+        slot0.unlocked = true;
+    }
+
+
+
+
+    constructor(
+        address _token0,
+        address _token1,
+        uint24 _fee,
+        int24 _tickSpacing
+    ){
+        token0 = _token0;
+        token1 =_token1;
+        fee = _fee;
+        tickSpacing = _tickSpacing;
+
+        maxLiquidityPerTick = Tick.tickSpacingToMaxLiquidityPerTick(_tickSpacing);
+    }
+
+    
+        function initialize(uint160 sqrtPriceX96) external{
+            require(slot0.sqrtPriceX96 == 0, "Already Initialized");
+            int24 tick = TickMath.getTickAtSqrtRatio(sqrtPriceX96);
+
+            slot0 = Slot0({
+                sqrtPriceX96: sqrtPriceX96,
+                tick: tick,
+                unlocked: true
+
+            });
+        } 
+
+
+        struct ModifyPositionParams{
+            address owner;
+            int24 tickLower;
+            int24 tickUpper;
+            int128 liquidityDelta;
+        }
+
+        function _updatePosition(
+            address owner,
+            int24 tickLower,
+            int24 tickUpper,
+            int128 liquidityDelta,
+            int24 tick
+        ) private returns (Position.Info storage position){
+            position = positions.get(owner, tickLower, tickUpper);
+
+            uint256 _feeGrowthGlobal0X128 = 0; 
+            uint256 _feeGrowthGlobal1X128 = 0;
+
+            bool flippedLower;
+            bool flippedUpper;
+            if(liquidityDelta != 0){
+                flippedLower = ticks.update(
+                    tickLower, 
+                    tick, 
+                    liquidityDelta, 
+                    _feeGrowthGlobal0X128, 
+                    _feeGrowthGlobal1X128, 
+                    false, 
+                    maxLiquidityPerTick
+                );
+
+                flippedUpper = ticks.update(
+                    tickUpper, 
+                    tick, 
+                    liquidityDelta, 
+                    _feeGrowthGlobal0X128, 
+                    _feeGrowthGlobal1X128, 
+                    true, 
+                    maxLiquidityPerTick
+                );
+
+                if(flippedLower){
+                    tickBitmap.flipTick(tickLower, tickSpacing);
+                }
+                if(flippedUpper){
+                    tickBitmap.flipTick(tickUpper, tickSpacing);
+                }
+            }
+
+            position.update(liquidityDelta, 0, 0);
+
+            if (liquidityDelta < 0) {
+            if (flippedLower) {
+                ticks.clear(tickLower);
+            }
+            if (flippedUpper) {
+                ticks.clear(tickUpper);
+            }
+        }
+    }
+
+        function _modifyPosition( ModifyPositionParams memory params) private returns ( 
+            Position.Info storage position,
+            int256 amount0,
+            int256 amount1
+            ){  
+                checkTicks(params.tickLower, params.tickUpper);
+                Slot0 memory _slot0 = slot0;
+
+            position = _updatePosition(
+                params.owner,
+                params.tickLower,
+                params.tickUpper,
+                params.liquidityDelta,
+                _slot0.tick
+            );
+
+            if(params.liquidityDelta != 0){
+                if(_slot0.tick < params.tickLower){
+
+                    amount0 = SqrtPriceMath.getAmount0Delta(
+                        TickMath.getSqrtRatioAtTick(params.tickLower),
+                        TickMath.getSqrtRatioAtTick(params.tickUpper),
+                        params.liquidityDelta
+                    );
+                } else if (_slot0.tick < params.tickUpper){
+
+                    amount0 = SqrtPriceMath.getAmount0Delta(
+                        _slot0.sqrtPriceX96,
+                        TickMath.getSqrtRatioAtTick(params.tickUpper),
+                        params.liquidityDelta
+                    );
+                    amount1 = SqrtPriceMath.getAmount1Delta(
+                        TickMath.getSqrtRatioAtTick(params.tickLower),
+                        _slot0.sqrtPriceX96,
+                        params.liquidityDelta
+                    );
+
+                    liquidity = params.liquidityDelta < 0
+                    ? liquidity - uint128( -params.liquidityDelta)
+                    : liquidity + uint128(params.liquidityDelta);
+
+                } else {
+                    amount1 = SqrtPriceMath.getAmount1Delta(
+                        TickMath.getSqrtRatioAtTick(params.tickLower),
+                        TickMath.getSqrtRatioAtTick(params.tickUpper),
+                        params.liquidityDelta
+                    );
+
+                }
+            }
+        }
+        
+
+        function mint(
+            address recepient, 
+            int24 tickLower, 
+            int24 tickUpper, 
+            uint128 amount) external lock returns (uint256 amount0, uint256 amount1){
+                require(amount > 0);
+                (, int256 amount0Int, int256 amount1Int) = 
+                _modifyPosition(
+                    ModifyPositionParams({
+                        owner: recepient,
+                        tickLower: tickLower,
+                        tickUpper: tickUpper,
+                        liquidityDelta: int256(uint256(amount)).toInt128()
+                    })
+                );
+
+                amount0 = uint256(amount0Int);
+                amount1 = uint256(amount1Int);
+
+                if(amount0 > 0){
+                    IERC20(token0).transferFrom(msg.sender, address(this), amount0);
+                }
+
+                if(amount1> 0){
+                    IERC20(token1).transferFrom(msg.sender, address(this), amount1);
+                }
+
+        }
+
+        function collect(
+            address recipient,
+            int24 tickLower,
+            int24 tickUpper, 
+            uint128 amount0Requested,
+            uint128 amount1Requested
+        ) external lock returns( uint128 amount0, uint128 amount1){
+            Position.Info storage position = positions.get(msg.sender, tickLower, tickUpper);
+
+            amount0 = amount0Requested > position.tokensOwed0 ? position.tokensOwed0 : amount0Requested;
+            amount1 = amount1Requested > position.tokensOwed1 ? position.tokensOwed1 : amount1Requested;
+
+            if(amount0 > 0){
+                position.tokensOwed0 -= amount0;
+                IERC20(token0).transfer(recipient, amount0);
+            } 
+            if(amount1 > 0){
+                position.tokensOwed1 -= amount1;
+                IERC20(token1).transfer(recipient, amount1);
+            }
+            
+        }
+
+        function burn( int24 tickLower, int24 tickUpper, uint128 amount) 
+        external lock returns( uint256 amount0, uint256 amount1) {
+            ( Position.Info storage position, int256 amount0Int, int256 amount1Int) =_modifyPosition(
+                ModifyPositionParams({
+                    owner: msg.sender,
+                    tickLower: tickLower,
+                    tickUpper: tickUpper,
+                    liquidityDelta: -int256(uint256(amount)).toInt128()
+
+                })
+                );
+
+            amount0 = uint256(-amount0Int);
+            amount1 = uint256(-amount1Int);
+
+            if(amount0 > 0 || amount1 > 0){
+                (position.tokensOwed0, position.tokensOwed1) = (
+                    position.tokensOwed0 + uint128(amount0),
+                    position.tokensOwed1 + uint128(amount1)
+                );
+            }
+            
+        }
+
+    struct SwapCache {
+            
+            uint128 liquidityStart;
+           
+        }
+
+        struct SwapState {
+            int256 amountSpecifiedRemaining;
+            int256 amountCalculated;
+            uint160 sqrtPriceX96;
+            int24 tick;
+            uint256 feeGrowthGlobalX128;
+            uint128 liquidity;
+        }
+
+        struct StepComputations {
+            uint160 sqrtPriceStartX96;
+            int24 tickNext;
+            bool initialized;
+            uint160 sqrtPriceNextX96;
+            uint256 amountIn;
+            uint256 amountOut;
+            uint256 feeAmount;
+        }
+
+    function swap(
+            address recipient,
+            bool zeroForOne,
+            int256 amountSpecified,
+            uint160 sqrtPriceLimitX96
+        ) external lock returns (int256 amount0, int256 amount1) {
+            require(amountSpecified !=0 , "INVALID AMOUNT");
+
+            Slot0 memory slot0Start = slot0;
+
+            require(
+                zeroForOne
+                    ? sqrtPriceLimitX96 < slot0Start.sqrtPriceX96 && sqrtPriceLimitX96 > TickMath.MIN_SQRT_RATIO
+                    : sqrtPriceLimitX96 > slot0Start.sqrtPriceX96 && sqrtPriceLimitX96 < TickMath.MAX_SQRT_RATIO, "invalid sqrtPrice limit"
+                );
+
+            SwapCache memory cache = SwapCache(liquidity);
+
+            bool exactInput = amountSpecified > 0;
+
+            SwapState memory state =
+            SwapState({
+                amountSpecifiedRemaining: amountSpecified,
+                amountCalculated: 0,
+                sqrtPriceX96: slot0Start.sqrtPriceX96,
+                tick: slot0Start.tick,
+                feeGrowthGlobalX128: zeroForOne ? feeGrowthGlobal0X128 : feeGrowthGlobal1X128,
+                liquidity: cache.liquidityStart
+            });
+
+            //TO-DO 
+            while(state.amountSpecifiedRemaining != 0 && state.sqrtPriceX96 != sqrtPriceLimitX96) {
+                StepComputations memory step;
+
+                step.sqrtPriceStartX96 = state.sqrtPriceX96;
+                
+                (step.tickNext, step.initialized)  = tickBitmap.nextInitializedTickWithinOneWord(
+                    state.tick, 
+                    tickSpacing, 
+                    zeroForOne);
+                if(step.tickNext < TickMath.MIN_TICK){
+                    step.tickNext = TickMath.MIN_TICK;
+                } else if ( step.tickNext > TickMath.MAX_TICK){
+                    step.tickNext = TickMath.MAX_TICK;
+                }
+
+                step.sqrtPriceNextX96 = TickMath.getSqrtRatioAtTick(step.tickNext);
+
+                (state.sqrtPriceX96, step.amountIn, step.amountOut, step.feeAmount) = SwapMath.computeSwapStep(
+                state.sqrtPriceX96,
+                (zeroForOne ? step.sqrtPriceNextX96 < sqrtPriceLimitX96 : step.sqrtPriceNextX96 > sqrtPriceLimitX96)
+                    ? sqrtPriceLimitX96
+                    : step.sqrtPriceNextX96,
+                state.liquidity,
+                state.amountSpecifiedRemaining,
+                fee
+                );
+                if (exactInput) {
+                    state.amountSpecifiedRemaining -= (step.amountIn + step.feeAmount).toInt256();
+                    state.amountCalculated -= step.amountOut.toInt256();
+                } else {
+                    state.amountSpecifiedRemaining += step.amountOut.toInt256();
+                    state.amountCalculated = (step.amountIn + step.feeAmount).toInt256();
+                }
+
+                //TO-DO update global fee tracker
+                if (state.sqrtPriceX96 == step.sqrtPriceNextX96) {
+                    if(step.initialized){
+                        int128 liquidityNet = ticks.cross(
+                            step.tickNext,
+                            zeroForOne
+                            ? state.feeGrowthGlobalX128
+                            : feeGrowthGlobal0X128,
+                            zeroForOne
+                            ? feeGrowthGlobal1X128
+                            : state.feeGrowthGlobalX128
+                        );
+                    if (zeroForOne) {
+                        liquidityNet = -liquidityNet;
+                    }
+                    state.liquidity = liquidity < 0
+                        ? state.liquidity - uint128(-liquidityNet)
+                        : state.liquidity + uint128(liquidityNet);
+                    }
+                    state.tick = zeroForOne ? step.tickNext - 1
+                    : step.tickNext;
+                } else if (state.sqrtPriceX96 != step.sqrtPriceStartX96){
+                    state.tick = TickMath.getTickAtSqrtRatio(state.sqrtPriceX96);
+                }
+
+
+            }
+
+            if(state.tick != slot0Start.tick){
+                (slot0.sqrtPriceX96, slot0.tick) = (state.sqrtPriceX96, state.tick);
+            } else {
+                slot0.sqrtPriceX96 = state.sqrtPriceX96;
+            }
+
+            if(cache.liquidityStart != state.liquidity){
+                liquidity = state.liquidity;
+            }
+
+            if(zeroForOne){
+                feeGrowthGlobal0X128 = state.feeGrowthGlobalX128;
+            } else {
+                feeGrowthGlobal1X128 = state.feeGrowthGlobalX128;
+            }
+
+            (amount0, amount1) = zeroForOne == exactInput
+            ? (amountSpecified - state.amountSpecifiedRemaining, state.amountCalculated)
+            : (state.amountCalculated, amountSpecified - state.amountSpecifiedRemaining);
+
+            if(zeroForOne){
+                if(amount1 < 0){
+                    IERC20(token1).transfer(recipient, uint256(-amount1));
+                    IERC20(token0).transferFrom(msg.sender, address(this), uint256(amount0));
+                }
+            } else{
+                if(amount0 < 0){
+                    IERC20(token0).transfer(recipient, uint256(-amount0));
+                    IERC20(token1).transferFrom(msg.sender, address(this), uint256(amount1));
+                }
+            }
+            
+        }
+
+}
+
---
+++ src/lib/Tick.sol
@@ -0,0 +1,81 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import "./TickMath.sol";
+
+
+
+library Tick {
+
+    
+struct Info {
+        uint128 liquidityGross;        
+        int128 liquidityNet;        
+        uint256 feeGrowthOutside0X128;
+        uint256 feeGrowthOutside1X128;        
+        bool initialized;    
+}
+        function tickSpacingToMaxLiquidityPerTick(int24 tickSpacing) internal pure returns (uint128) {
+        int24 minTick = (TickMath.MIN_TICK / tickSpacing) * tickSpacing;
+        int24 maxTick = (TickMath.MAX_TICK / tickSpacing) * tickSpacing;
+        uint24 numTicks = uint24((maxTick - minTick) / tickSpacing) + 1;
+        return type(uint128).max / numTicks;
+
+        }
+
+        function update(
+        mapping(int24 => Tick.Info) storage self,
+        int24 tick,
+        int24 tickCurrent,
+        int128 liquidityDelta,
+        uint256 feeGrowthGlobal0X128,
+        uint256 feeGrowthGlobal1X128,
+        //uint160 secondsPerLiquidityCumulativeX128,
+        //int56 tickCumulative,
+        //uint32 time,
+        bool upper,
+        uint128 maxLiquidity
+    ) internal returns (bool flipped){
+        Tick.Info storage info = self[tick];
+
+        uint128 liquidityGrossBefore = info.liquidityGross;
+        uint128 liquidityGrossAfter = liquidityDelta < 0
+        ? liquidityGrossBefore - uint128(-liquidityDelta)
+        : liquidityGrossBefore + uint128(liquidityDelta);
+
+        require(liquidityGrossAfter <= maxLiquidity, 'Liquidity Over the maximum');
+
+        flipped = (liquidityGrossAfter == 0) != (liquidityGrossBefore == 0);
+
+        if(liquidityGrossBefore == 0){
+            info.initialized = true;
+
+        }
+
+        info.liquidityGross = liquidityGrossAfter;
+
+        info.liquidityNet = upper 
+        ? info.liquidityNet - liquidityDelta
+        : info.liquidityNet + liquidityDelta;        
+    }
+
+    function clear(mapping(int24 => Info) storage self, int24 tick) internal {
+        delete self[tick];
+    }
+
+
+    function cross(
+        mapping(int24 => Tick.Info) storage self,
+        int24 tick,
+        uint256 feeGrowthGlobal0X128,
+        uint256 feeGrowthGlobal1X128
+        ) internal returns (int128 liquidityNet) {
+            Tick.Info storage info = self[tick];
+            info.feeGrowthOutside0X128 = feeGrowthGlobal0X128 - info.feeGrowthOutside0X128;
+            info.feeGrowthOutside1X128 = feeGrowthGlobal1X128 - info.feeGrowthOutside1X128;
+            liquidityNet = info.liquidityNet;
+
+    }
+
+
+}
